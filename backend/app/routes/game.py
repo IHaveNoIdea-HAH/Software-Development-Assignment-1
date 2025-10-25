ee1f81ae34789022b979625b067ad26ecca819ad
IHaveNoIdea-HAH <juanillocedric@gmail.com>
2025-10-25 18:44:53 +0100

    Made testing for randomly displaying words
    In the end I had to learn more about json, javascript and such.

--- backend/app/routes/game.py
+++ backend/app/routes/game.py
@@ -1,12 +1,15 @@
-from flask import Blueprint, request, jsonify, current_app
-from app.services.crossword_service import CrosswordService
-from app.schemas.crossword_schema import CrosswordSchema
+from flask import Blueprint, request, jsonify, current_app, render_template
+from app.services.crossword_service import CrosswordService 
+from app.schemas.crossword_schema import CrosswordSchema 
 from app.models.game import Game
 from app.models.crossword import Crossword
+import json
+import random
+import os
 
 game_bp = Blueprint('game', __name__)
 
-@game_bp.route('/start', methods=['POST'])
+@game_bp.route('/start', methods=['POST']) #to access these you must do /api/game/[route] (example: http://192.168.0.15:5000/api/game/welcome)
 def start_game():
     try:
         # Let's retrieve the JSON payload from the request body of the POST request
@@ -72,3 +75,23 @@ def game_status(game_id):
 def game_welcome():
     # Placeholder: Returns welcome message
     return jsonify({'welcome_message': 'Hello and welcome to our cool crossword game', 'status': 'running'})
+
+@game_bp.route('/test', methods=['GET'])
+def game_test():
+    return render_template('Test.html') #Remember to change the html to something else
+
+@game_bp.route('/random-word', methods=['GET']) #Gets a random word
+def random_word():
+    json_path = os.path.join(current_app.root_path, 'data', 'crossword_words.json') #Gets words from data folder, crossword json file
+    try:
+        with open(json_path, 'r') as f: #Loads json
+            words = json.load(f)
+        
+        if words: #If the word has been located choose a random word from there
+            chosen = random.choice(words)
+            return jsonify(chosen), 200
+        else:
+            return jsonify({'error': 'No words found'}), 404 #If no words has been found, cause error.
+    except FileNotFoundError: #Ensures we input json file in the correct place
+        return jsonify({'error': 'JSON file not found at ' + json_path}), 404
+   
